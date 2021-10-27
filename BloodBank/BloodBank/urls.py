6c66ff263b57bd61513af3d9f70985c91c9d9c46
michaelrilan <micorilan1999@gmail.com>
2021-10-27 15:13:57 +0800

    Templates/activities

--- BloodBank/BloodBank/urls.py
+++ BloodBank/BloodBank/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('BloodBanksys/', include('BlookBanksys.urls')),
+    path('BloodBanksys/', include('BloodBanksys.urls')),
     path('admin/', admin.site.urls),
 ]
 
