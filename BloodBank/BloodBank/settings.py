3ea37d8468ff12a20d32cd92a8d9e68a0ce7e001
Sean032100 <seandavidchristopher.monacillo@gsfe.tupcavite.edu.ph>
2021-10-27 15:02:12 +0800

    commit

--- BloodBank/BloodBank/settings.py
+++ BloodBank/BloodBank/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2-(avwu*st6=8#_87e3h=!nq9xaq$ruk*$ewsf--#u!@m@&iw6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
