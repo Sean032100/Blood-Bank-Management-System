c87c723531f7b3f906fccd50bb6e602d3d217eae
Nigelle <91709880+Nigelle@users.noreply.github.com>
2021-10-27 15:00:53 +0800

    Update views.py

--- BloodBank/BloodBanksys/views.py
+++ BloodBank/BloodBanksys/views.py
@@ -1,9 +1,7 @@
-from django.shortcuts import render
-
 # Create your views here.
 
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Welcome to web development 2')
+    return render(request, 'activities/index.html')
